826b322dcdd831c0e2d5f8cdbb94f8fda5ae7222
Kristine Morinaka <kfmorinaka@gmail.com>
2015-10-21 17:38:42 -0700

    update for deployment

--- config.rb
+++ config.rb
@@ -70,3 +70,11 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# GitHub Pages deployment configuration
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+end
+
+  
